ad7bd6f9c79169ed74509e75f613ff789a0a5ae0
sweetmangoes <alphonsechristian@gmail.com>
2023-06-05 18:34:06 -0400

    made changes, cleanup code

---
+++ metrofit-landingpage/src/app/hello/page.js
@@ -0,0 +1,7 @@
+export default function Hello(){
+  return (
+    <>
+      Hello from the Hello page!
+    </>
+  )
+}
--- metrofit-landingpage/src/components/Form.jsx
+++ metrofit-landingpage/src/components/Form.jsx
@@ -1,20 +1,20 @@
 export default function Form() {
   return (
-    <form action="/api/form" method="post">
+    <form className="text-black" action="/api/form" method="post">
       <label htmlFor="first">First Name</label>
       <input type="text" id="first" name="first" required />
  
       <label htmlFor="last">Last Name</label>
       <input type="text" id="last" name="last" required />
 
-      <label htmlFor="business">Business Name</label>
+      {/* <label htmlFor="business">Business Name</label>
       <input type="text" id="business" name="business" required />
 
       <label htmlFor="email"> Business email</label>
       <input type="text" id="email" name="email" required />
 
       <label htmlFor="phone">Phone Number</label>
-      <input type="number" id="phone" name="phone" required />
+      <input type="number" id="phone" name="phone" required /> */}
  
       <button type="submit">Submit</button>
     </form>

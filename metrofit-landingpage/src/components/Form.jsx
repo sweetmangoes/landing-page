a3ce4300c710e5212e50a25941d226509870ecd8
sweetmangoes <alphonsechristian@gmail.com>
2023-06-05 18:09:41 -0400

    added more input and labels for business name, phone, etc.

--- metrofit-landingpage/src/components/Form.jsx
+++ metrofit-landingpage/src/components/Form.jsx
@@ -6,6 +6,15 @@ export default function Form() {
  
       <label htmlFor="last">Last Name</label>
       <input type="text" id="last" name="last" required />
+
+      <label htmlFor="business">Business Name</label>
+      <input type="text" id="business" name="business" required />
+
+      <label htmlFor="email"> Business email</label>
+      <input type="text" id="email" name="email" required />
+
+      <label htmlFor="phone">Phone Number</label>
+      <input type="number" id="phone" name="phone" required />
  
       <button type="submit">Submit</button>
     </form>

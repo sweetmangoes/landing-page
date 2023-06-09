cb14369bf59659fa3f2dade0437d7d13ccfa71b5
sweetmangoes <alphonsechristian@gmail.com>
2023-06-09 17:57:47 -0400

    fix style for Mission.js

--- metrofit-landing/components/Mission.js
+++ metrofit-landing/components/Mission.js
@@ -14,19 +14,19 @@ export default function Mission() {
         </p>
         <hr className="border-black border-2" />
         <h3 className="font-monomaniac text-[5rem] leading-[5rem] mb-5 mt-3">VISION</h3>
-        <p className="font-montserrat text-2xl mb-5">
+        <p className="font-montserrat text-2xl">
           Our vision is to build a stronger and healthier city by helping people achieve fitness goals while supporting local fitness instructors, gyms, and studios.
           <br /><br />
           <i>We do this by having a platform that connects people with individual gyms and fitness classes in a seamless and convenient way.</i>
           <br /><br />
-          <ul className="list-disc list-inside">
+        </p>
+          <ul className="list-disc list-inside text-2xl mb-5 font-montserrat">
             Affordable and flexible gym classes benefit both parties:
             <li className="mt-1">People seeking such classes</li>
             <li>
               Gyms/instructors aiming to <span className="underline underline-offset-4">increase revenue</span> and <span className="underline underline-offset-4">build a loyal client base.</span>
             </li>
           </ul>
-        </p>
         <hr className="border-black border-2" />
       </div>
     </div>

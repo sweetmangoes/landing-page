dad850da9b9c648763e54deebfd8906be9593374
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-06-09 12:54:59 -0600

    Sizing change for hero

--- metrofit-landing/components/Hero.js
+++ metrofit-landing/components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero() {
 
   return (
-    <div className="bg-black h-[80vh] w-full p-6 flex">
+    <div className="bg-black h-[90vh] w-full p-6 flex">
       <div className="bg-blue-500 h-full w-1/2 px-14 flex flex-col justify-center">
         <h1 className="text-9xl mb-3">Metro<br />Fit</h1>
         <p className="text-3xl my-3">Join The Movement Community.</p>

ae23962389b7cd52d673d560ce2f7a7e67f23703
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-06-09 12:53:26 -0600

    Finished text for hero

--- metrofit-landing/components/Hero.js
+++ metrofit-landing/components/Hero.js
@@ -1,8 +1,13 @@
 export default function Hero() {
 
   return (
-    <div className="bg-red-500 h-[80vh] w-full">
-      <div>Hello</div>
+    <div className="bg-black h-[80vh] w-full p-6 flex">
+      <div className="bg-blue-500 h-full w-1/2 px-14 flex flex-col justify-center">
+        <h1 className="text-9xl mb-3">Metro<br />Fit</h1>
+        <p className="text-3xl my-3">Join The Movement Community.</p>
+        <button className="py-2 px-6 border-2 border-[#e0e111] text-[#e0e111] w-1/5">JOIN WAITLIST</button>
+      </div>
+      <div className="bg-green-500 h-full w-1/2">Hello</div>
     </div>
   )
 }

dbf57b3b9c9d2847ed6e6bfc11eeeb67c7f2c55e
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-06-09 16:38:06 -0600

    Responsive design for hero

--- metrofit-landing/components/Hero.js
+++ metrofit-landing/components/Hero.js
@@ -1,14 +1,27 @@
 export default function Hero() {
 
   return (
-    <div className="bg-black h-[90vh] w-full p-6 flex">
-      <div className="w-1/2 px-10 flex flex-col pt-6">
-        <h1 className="font-monomaniac text-[20rem] leading-[16rem]">Metro</h1>
-        <h1 className="font-monomaniac text-[20rem] leading-[16rem] mb-14">Fit</h1>
-        <p className="text-[3.1rem] leading-[3.1rem] mt-3 mb-8 font-montserrat">Join The <span className="underline underline-offset-[8px]">Movement Community.</span></p>
-        <button className="py-4 border-4 border-[#e0e111] text-[#e0e111] text-5xl w-4/6 font-montserrat font-bold">SECURE YOUR SPOT</button>
+    <div className="bg-black w-full flex flex-col
+                    lg:p-6 lg:flex-row lg:h-[90vh]">
+      <div className="w-full flex flex-col pt-6 pl-5 
+                      lg:w-1/2 lg:px-10">
+        <h1 className="font-monomaniac text-[10rem] leading-[8rem] 
+                       lg:text-[12rem] lg:leading-[10rem] 
+                       xl:text-[16rem] xl:leading-[12rem]">
+          Metro
+        </h1>
+        <h1 className="font-monomaniac text-[10rem] leading-[8rem] mb-14 
+                       lg:text-[14rem] lg:leading-[10rem] lg:mb-14 
+                       xl:text-[16rem] xl:leading-[12rem]">
+          Fit
+        </h1>
+        <p className="mt-3 mb-8 font-montserrat text-4xl
+                      lg:text-[3.1rem] lg:leading-[3.1rem]">
+          Join The <span className="underline underline-offset-[8px]">Movement Community.</span>
+        </p>
+        <button className="py-4 border-4 border-[#e0e111] text-[#e0e111] w-1/2 text-2xl font-montserrat font-bold lg:text-4xl xl:text-5xl lg:w-4/6">SECURE YOUR SPOT</button>
       </div>
-      <div className="h-full w-1/2 flex justify-center">
+      <div className="h-full w-full flex items-center justify-center lg:w-1/2">
         <img src='/images/hero.svg'></img>
       </div>
     </div>

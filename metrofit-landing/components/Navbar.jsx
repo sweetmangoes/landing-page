34428de10b5990a06856419c028fd3bbd75649e6
sweetmangoes <alphonsechristian@gmail.com>
2023-06-10 13:26:32 -0400

    added styling for mobile

--- metrofit-landing/components/Navbar.jsx
+++ metrofit-landing/components/Navbar.jsx
@@ -4,43 +4,45 @@ import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai"
 
 export default function Navbar(){
   return (
-    <div> 
-      <div>
+    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300"> 
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4">
         <Link href='/'>  
-          <h1>Metro Fit</h1>
+          <h1 className="font-monomaniac text-4xl">
+            Metro Fit
+          </h1>
         </Link>
-        <ul>
-          <li>
+        <ul className="hidden sm:flex ">
+          <li className="p-4 font-monomaniac">
             <Link href='/'>Home</Link>
           </li>
-          <li>
+          <li className="p-4 font-monomaniac">
             <Link href='/partner'>Be a Partner</Link>
           </li>
-          <li>
+          <li className="p-4 font-monomaniac">
             <Link href='/join'>Join</Link>
           </li>
-          <li>
+          <li className="p-4 font-monomaniac">
             <Link href='/contact'>Contact</Link>
           </li>
         </ul>
 
         {/* Mobile Button */}
-        <div>
+        <div className="block sm:hidden z-10">
           <AiOutlineMenu size={20}/>
         </div>
         {/* Mobile Menu */}
-        <div>
+        <div className="sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300">
         <ul>
-          <li>
+          <li className="p-4 font-monomaniac text-4xl hover:text-gray-500">
             <Link href='/'>Home</Link>
           </li>
-          <li>
+          <li className="p-4 font-monomaniac text-4xl hover:text-gray-500">
             <Link href='/partner'>Be a Partner</Link>
           </li>
-          <li>
+          <li className="p-4 font-monomaniac text-4xl hover:text-gray-500">
             <Link href='/join'>Join</Link>
           </li>
-          <li>
+          <li className="p-4 font-monomaniac text-4xl hover:text-gray-500">
             <Link href='/contact'>Contact</Link>
           </li>
         </ul>

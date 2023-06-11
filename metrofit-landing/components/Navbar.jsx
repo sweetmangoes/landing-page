b09a0eaff002cf29bfed7db9934468e321780c8c
sweetmangoes <alphonsechristian@gmail.com>
2023-06-11 00:17:57 -0400

    added useEffect for interactivity

--- metrofit-landing/components/Navbar.jsx
+++ metrofit-landing/components/Navbar.jsx
@@ -1,26 +1,44 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Link from "next/link";
 import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai"
 
 export default function Navbar(){
 
   const [nav, setNav] = useState(false)
+  const [color, setColor] = useState('transparent')
+  const [textColor, setTextColor] = useState('white')
 
   const handleNav = () => {
     setNav(!nav)
   }
 
+  useEffect(() => {
+    const changeColor = () =>{
+      if (window.scrollY >= 90){
+        setColor('#ffffff');
+        setTextColor('#00000')
+      }else{
+        setColor('Transparent')
+        setTextColor('#ffffff')
+      }
+    }
+  })
+
   return (
-    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300"> 
+    <div style={{backgroundColor: `${color}`}} className="fixed left-0 top-0 w-full z-10 ease-in duration-300"> 
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4">
         <Link href='/'>  
-          <h1 className="font-monomaniac text-4xl">
+          <h1 style={{color:`${textColor}`}} className="font-monomaniac text-4xl">
             Metro Fit
           </h1>
         </Link>
-        <ul className="hidden sm:flex ">
-          <li className="p-4 font-monomaniac text-3xl">
-            <Link href='/'>Home</Link>
+        <ul style={{color:`${textColor}`}}  className="hidden sm:flex ">
+          <li>
+            <Link href='/'>
+              <h1 className="p-4 font-monomaniac text-3xl">
+                Home
+              </h1>
+            </Link>
           </li>
           <li className="p-4 font-monomaniac text-3xl">
             <Link href='/partner'>Be a Partner</Link>
@@ -35,7 +53,7 @@ export default function Navbar(){
 
         {/* Mobile Button */}
         <div className="block sm:hidden z-10" onClick={handleNav}>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+          {nav ? <AiOutlineClose style={{color:`${textColor}`}}  size={20}/> : <AiOutlineMenu style={{color:`${textColor}`}}  size={20}/>}
         </div>
         {/* Mobile Menu */}
         <div className= {

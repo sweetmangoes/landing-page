f3579e432c33b6aa9a025751e986f60fbc87a44f
sweetmangoes <alphonsechristian@gmail.com>
2023-06-29 23:23:10 -0400

    added corner to button

--- metrofit-landing/components/Member.js
+++ metrofit-landing/components/Member.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Member() {
 
   return (
@@ -11,11 +13,12 @@ export default function Member() {
         <div className="w-[49.3%] h-full rounded-[3rem] p-5 flex flex-col">
           <p className="font-montserrat text-[4.5rem] leading-[6rem] text-black p-8"><b>Affordable,<br />accessible,<br />and flexible<br />classes for all.</b></p>
           <div className="flex mt-[6rem]">
-            <button className="py-4 bg-black text-[#fefefe] w-1/2 text-2xl font-montserrat font-bold ml-[2.5rem]
+            <Link href='./waitlist' className="py-4 rounded-lg flex justify-center hover:bg-amber-300 hover:text-black bg-black text-[#fefefe] w-1/2 text-2xl font-montserrat font-bold ml-[2.5rem]
                            lg:text-3xl lg:w-1/3
                            xl:text-2xl">
-              BE A PARTNER
-            </button>
+            
+            BE A PARTNER
+            </Link>
           </div>
         </div>
       </div>

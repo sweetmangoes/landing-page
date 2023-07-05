c266cb74d8d4f09e20365699c3c14f2b224fe17a
sweetmangoes <alphonsechristian@gmail.com>
2023-07-04 20:10:07 -0400

    added flex

--- metrofit-landing/pages/partner/index.js
+++ metrofit-landing/pages/partner/index.js
@@ -4,7 +4,7 @@ import Form from "../../components/Form"
 export default function Partner(){
   return(
     <div className="bg-white w-full flex flex-row items-center justify-evenly lg:h-[90vh]">
-      <div className="border-[3px] rounded-[3rem] border-black h-[90%] w-[94%] flex">
+      <div className="border-[3px] rounded-[3rem] border-black h-[90%] w-[94%] flex justify-around">
         <div>
         <p className="text-black">
           hello from Partner!

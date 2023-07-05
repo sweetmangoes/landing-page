2260b114c3af7aaa0d83bd63bc8d5ff8c1a2f7fd
sweetmangoes <alphonsechristian@gmail.com>
2023-07-04 20:21:58 -0400

    added content

--- metrofit-landing/pages/partner/index.js
+++ metrofit-landing/pages/partner/index.js
@@ -6,9 +6,16 @@ export default function Partner(){
     <div className="bg-white w-full flex flex-row items-center justify-evenly lg:h-[90vh]">
       <div className="border-[3px] rounded-[3rem] border-black h-[90%] w-[94%] flex justify-around">
         <div>
-        <p className="text-black">
-          hello from Partner!
-        </p>
+          <h1 className="text-black font-monomaniac bg-amber-300 rounded-md text-lg p-1">
+            BE A PARTNER
+          </h1>
+          <img className="" src='/images/partnerlogo.svg'></img>
+          <h2 className="text-black">
+            increase booking revenue
+          </h2>
+          <p className="text-black">
+          Be our partner to earn cash by filling empty spots in classes. A partner Instructor can extra cash and schedule classes on their own time. 
+          </p>
         </div>
         <div>
           <Form />

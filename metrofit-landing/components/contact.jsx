1aae3268ac2bc0b65551a013504b5721a47ee5e5
sweetmangoes <alphonsechristian@gmail.com>
2023-07-05 02:10:03 -0400

    added styling and content

--- metrofit-landing/components/contact.jsx
+++ metrofit-landing/components/contact.jsx
@@ -1,33 +1,24 @@
 export default function Contact(){
   return(
-    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-full flex flex-col
+    <div className="bg-gradient-to-r from-pink-500 via-yellow-500 to-green-500 w-full flex flex-col
                     lg:h-[90vh]">
       <div className="w-full flex flex-col items-center justify-between p-10 font-montserrat h-4/5
                       lg:flex-row lg:h-4/5">
-        <div className="flex flex-col items-center text-center">
-          <img className="" src='/images/coin.svg'></img>
-          <p className="text-4xl lg:text-2xl mt-3">Increase<br />Booking</p>
-          <p className="text-xl lg:text-lg mt-4">Allow us to fill up your empty<br />classes and earn more revenue.</p>
+        <div className="flex flex-col">
+          <h1 className="text-4xl lg:text-2xl mt-3 text-black font-montserrat font-extrabold">
+            Help me empower the fitness community and build a stronger and healthier city. 
+          </h1>
+          <p className="text-xl lg:text-lg mt-4 text-black font-montserrat">
+            To build a healthier and stronger city, we need to empower the fitness community by providing affordability, accessibility, and flexibility. 
+          </p>
+          <p className="text-xl lg:text-lg mt-4 text-black font-montserrat">
+            Contact me for the details: christian@curate.family
+          </p>
         </div>
-        <div className="flex flex-col items-center text-center">
-          <img className="" src='/images/mat.svg'></img>
-          <p className="text-4xl mt-3">Local gyms &<br />fitness studios</p>
-          <p className="text-xl mt-4">Be our partner to increase your booking<br />revenue and fill empty classes. </p>
+        <div cglassName="flex flex-col items-center text-center">
+          <img className="" src='/images/photocontact.svg'></img>
+          <p className="text-4xl mt-3 font-monomaniac text-black">Christian Alphonse, Founder</p>
         </div>
-        <div className="flex flex-col items-center text-center">
-          <img className="" src='/images/instructor.svg'></img>
-          <p className="text-4xl mt-3">Fitness<br />instructors</p>
-          <p className="text-xl mt-4">Be our partner to earn extra cash<br />and schedule classes on your time.</p>
-        </div>
-      </div>
-      <div className="h-1/5 w-full flex justify-center items-center
-                      lg:px-10">
-              {/* <Link className="flex justify-center py-4 border-4 mb-3 border-[#e0e111] text-[#e0e111] hover:bg-amber-300 hover:text-black w-1/2 text-2xl font-montserrat font-bold 
-            lg:text-3xl lg:w-1/3 
-            xl:text-2xl" href='/partner'>
-                BE A PARTNER
-              </Link> */}
-              
       </div>
     </div>
   )

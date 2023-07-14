74e75cb8b398432a51540552a695f1ad3da4b794
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-07-14 13:34:45 -0600

    Fix button error on xl format

--- metrofit-landing/components/Member.js
+++ metrofit-landing/components/Member.js
@@ -14,7 +14,7 @@ export default function Member() {
         <div className="invisible lg:visible lg:w-[0.4%] lg:h-[80%] lg:bg-black "><hr /></div>
         <div className="lg:w-[49.3%] h-full rounded-[3rem] p-5 flex flex-col text-center justify-center lg:text-left">
           <p className="font-montserrat text-[3rem] leading-[2.5rem]  lg:text-[4.5rem] lg:leading-[4.5rem] text-black p-8"><b>Affordable,<br />accessible,<br />and flexible<br />classes for all.</b></p>
-          <div className="flex mt-[6rem] justify-center">
+          <div className="flex mt-[6rem] justify-center xl:justify-start">
             <Link href='./waitlist' className="py-4 rounded-lg flex justify-center hover:bg-amber-300 hover:text-black bg-black text-[#fefefe] w-1/2 text-2xl font-montserrat font-bold ml-[2.5rem]
                            lg:text-3xl lg:w-2/3
                            xl:text-2xl xl:w-1/3">

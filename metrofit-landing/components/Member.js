0aa5bed6ca703489f41e7675cdb51bd4ce3c6a81
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-07-14 13:29:47 -0600

    fix styling on Member page

--- metrofit-landing/components/Member.js
+++ metrofit-landing/components/Member.js
@@ -3,22 +3,24 @@ import Link from "next/link";
 export default function Member() {
 
   return (
-    <div className="bg-white w-full flex flex-row items-center justify-center
+    <div className="bg-white w-full flex justify-center items-center
                     lg:h-[90vh]">
-      <div className="border-[3px] rounded-[3rem] border-black h-[90%] w-[94%] flex items-center">
-        <div className="w-[49.3%] h-full rounded-[3rem] bg-no-repeat bg-center bg-contain bg-[url('../public/images/member-bg.svg')]">
-          <p className="font-monomaniac text-[8rem] leading-[6rem] text-black p-8">BE A<br /> MEMBER</p>
+      <div className="h-full w-[94%] flex flex-col items-center p-5
+                      lg:border-[3px] lg:rounded-[3rem] lg:border-black lg:h-[90%] lg:flex-row lg:p-0">
+        <div className="h-full bg-no-repeat bg-center bg-contain bg-[url('../public/images/member-bg.svg')]
+                        lg:w-[49.3%] lg:rounded-[3rem]">
+          <p className="font-monomaniac text-[8rem] leading-[6rem] text-black p-8 mt-4 lg:mt-0">BE A<br /> MEMBER</p>
         </div>
-        <div className="w-[0.4%] h-[80%] bg-black"><hr /></div>
-        <div className="w-[49.3%] h-full rounded-[3rem] p-5 flex flex-col">
-          <p className="font-montserrat text-[4.5rem] leading-[6rem] text-black p-8"><b>Affordable,<br />accessible,<br />and flexible<br />classes for all.</b></p>
-          <div className="flex mt-[6rem]">
+        <div className="invisible lg:visible lg:w-[0.4%] lg:h-[80%] lg:bg-black "><hr /></div>
+        <div className="lg:w-[49.3%] h-full rounded-[3rem] p-5 flex flex-col text-center justify-center lg:text-left">
+          <p className="font-montserrat text-[3rem] leading-[2.5rem]  lg:text-[4.5rem] lg:leading-[4.5rem] text-black p-8"><b>Affordable,<br />accessible,<br />and flexible<br />classes for all.</b></p>
+          <div className="flex mt-[6rem] justify-center">
             <Link href='./waitlist' className="py-4 rounded-lg flex justify-center hover:bg-amber-300 hover:text-black bg-black text-[#fefefe] w-1/2 text-2xl font-montserrat font-bold ml-[2.5rem]
-                           lg:text-3xl lg:w-1/3
-                           xl:text-2xl">
-            JOIN WAITLIST
+                           lg:text-3xl lg:w-2/3
+                           xl:text-2xl xl:w-1/3">
+              JOIN WAITLIST
             </Link>
-           </div>
+          </div>
         </div>
       </div>
     </div>

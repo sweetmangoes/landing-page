ffe487ab3013e0d6fd76fb47721b38b0b9b8e242
sweetmangoes <alphonsechristian@gmail.com>
2023-08-15 21:52:57 -0400

    added space beneath

--- metrofit-landing/components/footer.jsx
+++ metrofit-landing/components/footer.jsx
@@ -8,7 +8,8 @@ export default function Footer(){
       </h1>
       <Link className='hover:text-amber-400 flex justify-center text-gray-400' href='https://app.termly.io/document/privacy-policy/2ab800f8-0f3f-4a5f-9150-aca7d84591e9'> 
         Learn more about our Privacy Policy 
-      </Link>         
+      </Link> 
+      <br/>        
     </div>
   )
 }

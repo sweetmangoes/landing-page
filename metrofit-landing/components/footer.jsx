33c8276a4204621fd5903b4406e6aa70ba4c1a66
sweetmangoes <alphonsechristian@gmail.com>
2023-08-15 21:47:13 -0400

    updated style

--- metrofit-landing/components/footer.jsx
+++ metrofit-landing/components/footer.jsx
@@ -2,17 +2,13 @@ import Link from "next/link";
 
 export default function Footer(){
   return(
-    <div className="bg-black flex flex-row justify-between space-x-96 text-white p-4 font-monomaniac text-lg">
-      <h1>
+    <div className="bg-black flex flex-col justify-center text-gray-300 font-monomaniac text-lg">
+      <h1 className="flex justify-center">
         MetroFit is a product of Curate Inc.
       </h1>
-      <div className=" flex items-center">
-          <div>
-          <Link className='hover:text-amber-400' href='https://app.termly.io/document/privacy-policy/2ab800f8-0f3f-4a5f-9150-aca7d84591e9'> 
-              Privacy Policy 
-            </Link>         
-          </div>
-      </div>
+      <Link className='hover:text-amber-400 flex justify-center text-gray-400' href='https://app.termly.io/document/privacy-policy/2ab800f8-0f3f-4a5f-9150-aca7d84591e9'> 
+        Learn more about our Privacy Policy 
+      </Link>         
     </div>
   )
 }

4f1a223e2f400d37d43df172e45960ab8d179341
sweetmangoes <alphonsechristian@gmail.com>
2023-08-25 00:15:23 -0400

    added hello console to api pages

--- metrofit-landing/pages/api/partnersubmit.js
+++ metrofit-landing/pages/api/partnersubmit.js
@@ -2,6 +2,8 @@ import { google } from 'googleapis';
 
 export default async function handler(req, res) {
 
+  console.log(`hello from partner submit`)
+
   if (req.method !== 'POST') {
     return res.status('405').send({ message: 'Only POST requests are allowed' });
   }

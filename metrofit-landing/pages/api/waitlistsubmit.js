92cfecc82f01c5b001d00c45ae4073423c92b342
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-08-24 23:23:24 -0600

    sheets object key log

--- metrofit-landing/pages/api/waitlistsubmit.js
+++ metrofit-landing/pages/api/waitlistsubmit.js
@@ -38,6 +38,8 @@ export default async function handler(req, res) {
       spreadsheetId: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID
     });
 
+    console.log(`sheets: ${Object.keys(sheets)}`);
+
     const response = await sheets.spreadsheets.values.append({
       spreadsheetId: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
       range: 'A1:D1',

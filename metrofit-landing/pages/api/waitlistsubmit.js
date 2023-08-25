30eb71ce651479c8d2861f82bd0950882096924d
Clayton Van Imschoot <clayton.vanimschoot@gmail.com>
2023-08-25 00:12:28 -0600

    Trying with replace

--- metrofit-landing/pages/api/waitlistsubmit.js
+++ metrofit-landing/pages/api/waitlistsubmit.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
-        private_key: `-----BEGIN PRIVATE KEY-----${b64_publicKey}-----END PRIVATE KEY-----`
+        private_key: `-----BEGIN PRIVATE KEY-----${b64_publicKey}-----END PRIVATE KEY-----`.replace(/\\n/g, '\n')
       },
       scopes: [
         'https://www.googleapis.com/auth/drive',
